4a63d52ad5a4c191c0ca757243b573699c202187
wangcong18 <wangcong18@xiaomi.com>
2020-07-03 20:02:37 +0800

    [Bug][Memroy Leak]Fix bug TransactionState is not clear from idToFinalStatusTransactionState
    This CL includes:
    1. Memory leak because transactionState is not removed.
    2. Extacting the clear logic to method to avoid forget.

--- fe/src/main/java/org/apache/doris/transaction/DatabaseTransactionMgr.java
+++ fe/src/main/java/org/apache/doris/transaction/DatabaseTransactionMgr.java
@@ -551,12 +551,7 @@ public class DatabaseTransactionMgr {
             if (!finalStatusTransactionStateDeque.isEmpty() &&
             transactionState.getTransactionId() == finalStatusTransactionStateDeque.getFirst().getTransactionId()) {
                 finalStatusTransactionStateDeque.pop();
-                idToFinalStatusTransactionState.remove(transactionState.getTransactionId());
-                Set<Long> txnIds = unprotectedGetTxnIdsByLabel(transactionState.getLabel());
-                txnIds.remove(transactionState.getTransactionId());
-                if (txnIds.isEmpty()) {
-                    labelToTxnIds.remove(transactionState.getLabel());
-                }
+                clearTransactionState(transactionState);
             }
         } finally {
             writeUnlock();
@@ -1010,11 +1005,7 @@ public class DatabaseTransactionMgr {
                 TransactionState transactionState = finalStatusTransactionStateDeque.getFirst();
                 if (transactionState.isExpired(currentMillis)) {
                     finalStatusTransactionStateDeque.pop();
-                    Set<Long> txnIds = unprotectedGetTxnIdsByLabel(transactionState.getLabel());
-                    txnIds.remove(transactionState.getTransactionId());
-                    if (txnIds.isEmpty()) {
-                        labelToTxnIds.remove(transactionState.getLabel());
-                    }
+                    clearTransactionState(transactionState);
                     editLog.logDeleteTransactionState(transactionState);
                     LOG.info("transaction [" + transactionState.getTransactionId() + "] is expired, remove it from transaction manager");
                 } else {
@@ -1024,6 +1015,15 @@ public class DatabaseTransactionMgr {
             }
         } finally {
             writeUnlock();
+        }
+    }
+
+    private void clearTransactionState(TransactionState transactionState) {
+        idToFinalStatusTransactionState.remove(transactionState.getTransactionId());
+        Set<Long> txnIds = unprotectedGetTxnIdsByLabel(transactionState.getLabel());
+        txnIds.remove(transactionState.getTransactionId());
+        if (txnIds.isEmpty()) {
+            labelToTxnIds.remove(transactionState.getLabel());
         }
     }
 
