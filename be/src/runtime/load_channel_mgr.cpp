12ab0a5ae58615115b34be727da3fde38ea09978
Yingchun Lai <405403881@qq.com>
2021-04-09 09:56:55 +0800

    format

--- be/src/runtime/load_channel_mgr.cpp
+++ be/src/runtime/load_channel_mgr.cpp
@@ -87,7 +87,7 @@ Status LoadChannelMgr::init(int64_t process_mem_limit) {
     int64_t load_mem_limit = calc_process_max_load_memory(process_mem_limit);
     _mem_tracker = MemTracker::CreateTracker(load_mem_limit, "Load");
     REGISTER_HOOK_METRIC(load_mem_consumption, [this]() {
-      return _mem_tracker->consumption();
+        return _mem_tracker->consumption();
     });
     RETURN_IF_ERROR(_start_bg_worker());
     return Status::OK();
